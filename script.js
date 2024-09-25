106dc25a159bcdb7c005b0b565935e54db1ae49a
king-sovan <156076876+king-sovan@users.noreply.github.com>
2024-09-25 12:24:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -7,3 +7,32 @@ const images = [
   { url: "https://picsum.photos/id/238/200/300" },
   { url: "https://picsum.photos/id/239/200/300" },
 ];
+
+const downloadImage = (images) => {
+	return new Promise((resolve, reject) => {
+		const img = new Image();
+		img.src = image.url;
+
+		img.onload = () => resolve(img);
+		img.onerror = () => reject(`Failed to load image's URL: ${image.url}` );
+	});
+};
+
+const displayImages = (images) => {
+	images.forEach((img) => {
+		output.appendchild(img);
+	});
+};
+
+btn.addEventListener("click", () => {
+	output.innerHTML = "";
+
+	Promise.all(images.map(downloadImage))
+		.then((downloadedImage) => {
+			displayImages(downloadedImage);
+		})
+		.catch((error) => {
+			console.error(error);
+			output.innerHTML = `<p style="color: red;">${error}</p>`;
+		});
+});
